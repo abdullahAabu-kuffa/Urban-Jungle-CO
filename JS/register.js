446346282cf8ba259b7f7fc8ce9732c277e86be5
Mustafa <mm0118250@gmail.com>
2025-08-12 16:32:47 +0300

    updation some validation on the register page

--- JS/register.js
+++ JS/register.js
@@ -18,30 +18,45 @@ function registeration() {
             return;
         }
         errorMessage.style.display = 'none';
+        name.style.border = '';
+        email.style.border = '';
+        password.style.border = '';
+        role.style.border = '';
+
         // validate form data email and password with regex
         var emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
         var passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
         if (!emailPattern.test(email.value.trim())) {
             document.querySelector('.errorEmail').style.display = 'block';
+            email.style.border = '1px solid red';
             document.querySelector('.errorEmail').textContent = 'Please enter a valid email address';
             return
         }
+        email.style.border = '';
+
         document.querySelector('.errorEmail').style.display = 'none';
         if (!passwordPattern.test(password.value.trim())) {
             document.querySelector('.errorPassword').style.display = 'block';
+            password.style.border = '1px solid red';
+
             document.querySelector('.errorPassword').textContent = 'Password must be at least 8 characters long and contain at least one letter and one number';
             return
         }
+        password.style.border = '';
+
         document.querySelector('.errorPassword').style.display = 'none';
 
         // validate if the email already exists
         var users = JSON.parse(localStorage.getItem('users') || '[]')
         var user = users.find(u => u.email === email.value)
         if (user) {
+            email.style.border = '1px solid red';
             document.querySelector('.errorEmail').style.display = 'block';
             document.querySelector('.errorEmail').textContent = 'Email already exists. Please use a different email.';
             return
         }
+        email.style.border = '';
+
         document.querySelector('.errorEmail').style.display = 'none';
 
         // save user data
